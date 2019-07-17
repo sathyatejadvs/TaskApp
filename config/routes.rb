69668e9727adcff3e7c79d59c79d2c3d88473fee
sathyatejadvs <daggubatisatyateja@gmail.com>
2019-07-17 17:51:33 +0530

    Refactored autherisation functionality
    - Implemented logout functionality when user is login.
    - Refactored the devise generated autherisation url( i.e,sign_in as
    login, signout as logout and signup as register).

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'pages/home'
-  get 'pages/about'
+	devise_for :users, path:'', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register' ,}
+
+	devise_scope :user do 
+		get "register", :to => "devise/registrations#new", as: :register 
+		get "login", :to => "devise/sessions#new", as: :login
+		get "logout", :to => "devise/sessions#destroy", as: :logout
+	end
+	
+	get 'pages/home'
+	get 'pages/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
 end
