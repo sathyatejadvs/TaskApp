3ddc301034a1c72ca1320112c13ca5629bb980a1
sathyatejadvs <daggubatisatyateja@gmail.com>
2019-07-18 17:13:31 +0530

    Refactored the navigation links
    - Integrated bootstrap styling for the navigation items
    - Refactored styling for view items
    - Added index action for blog controller
    - Created a blog layout file

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,14 @@
 class BlogsController < ApplicationController
+	before_action :set_album, only: [:show, :edit]
 	def index
+		@blog = Blog.all
+	end
+
+	def new
+		@blogs = Blog.new
+	end
+
+	def set_album
+		@blog = Blog.find(params[:id])
 	end
 end
