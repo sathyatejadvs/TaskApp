63e122e06d6ee6551a23c0e3a307472fc644efa0
sathyatejadvs <daggubatisatyateja@gmail.com>
2019-07-18 18:57:25 +0530

    Added create action to blog controller and added sample fields to bootstrap modal

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,12 +1,23 @@
 class BlogsController < ApplicationController
 	before_action :set_album, only: [:show, :edit]
 	def index
-		@blog = Blog.all
+		@blogs = Blog.all
 	end
 
 	def new
-		@blogs = Blog.new
+		@blog = Blog.new
 	end
+	def create
+    @blog = Blog.new(params.require(:blog).permit(:title, :description, :duedate))
+
+    respond_to do |format|
+      if @blog.save
+        format.html { redirect_to blogs_path, notice: 'Your Blog item is now live.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
 
 	def set_album
 		@blog = Blog.find(params[:id])
