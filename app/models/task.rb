572523a9f6c88caef88c5a98965b926045018397
sathyatejadvs <daggubatisatyateja@gmail.com>
2019-07-22 18:54:48 +0530

    Added a mongo attribute date_field to the Bootstrap modal
    - Integrated validations for the modal fields
    - Appended date in javascript file to work on index page

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,13 @@
 class Task
   include Mongoid::Document
+  # include Mongoid::Attributes::Dynamic
+
   field :title, type: String
   field :description, type: String
   field :due_date, type: Date
+  
   embedded_in :user
 
-   validates :title, presence: true
-   validates :description, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
 end
