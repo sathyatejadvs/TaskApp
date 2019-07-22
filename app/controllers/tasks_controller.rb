26839f0936d8ded8d82028869639b2decf7d0922
sathyatejadvs <daggubatisatyateja@gmail.com>
2019-07-22 16:44:27 +0530

    Refactored the javascript file to work

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,11 +28,12 @@ class TasksController < ApplicationController
 
     respond_to do |format|
       if @task.save
-        redirect_to 
         format.html { redirect_to @task, notice: 'Task was successfully created.' }
-        format.json { render :show, status: :created, location: @task }
+        format.js
+        format.js { render :show, status: :created, location: @task }
       else
         format.html { render :new }
+        format.js
         format.json { render json: @task.errors, status: :unprocessable_entity }
       end
     end
