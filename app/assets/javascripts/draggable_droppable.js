a35e6f94351e3366b50f4375fd25d7fcc6d50ea7
sathyatejadvs <daggubatisatyateja@gmail.com>
2019-07-24 19:24:47 +0530

    Refactored the drag and drop, create javascript

--- app/assets/javascripts/draggable_droppable.js
+++ app/assets/javascripts/draggable_droppable.js
@@ -1,14 +1,20 @@
+
 $(document).ready(function() {
 	
+
+
 	$(".draggable").draggable();
 	$(".droppable").droppable({
 
+
 		drop: function(event, ui) {
+		$(this).addClass("ui-state-highlight")
+
 		
 		},
 		over: function(event, ui) {
 				$('.droppable.ui-state-highlight').removeClass("ui-state-highlight")
-				$(this).addClass( "ui-state-highlight" )
+				$(this).addClass("ui-state-highlight")
 
 		}
 	});
