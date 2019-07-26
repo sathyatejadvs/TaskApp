e21ee1bfb451d3467128c4d07221abd4d7bfa162
sathyatejadvs <daggubatisatyateja@gmail.com>
2019-07-26 11:34:47 +0530

    Performing the tasks to be updated
    - Created a new file for update action.

--- app/assets/javascripts/draggable_droppable.js
+++ app/assets/javascripts/draggable_droppable.js
@@ -1,21 +1,28 @@
-
 $(document).ready(function() {
-	
 
+	dragnDrop();
 
+});
+
+function dragnDrop() {
 	$(".draggable").draggable( {
   	axis:'x'
 	});
 
 	$(".droppable").droppable({
 		drop: function(event, ui) {
-      console.log( 'Card was moved to '+ $(this).attr('id'))
-		},
+      console.log($(ui.draggable).find('.card-title').html() +' card is moved to '+ $(this).attr('id'))
+
+     var taskId = $(ui.draggable).data('task-id')
+     $.ajax({
+     	url: '/tasks/'+ taskId
+		}) 
+   },
+	
 		over: function(event, ui) {
 				$('.droppable.ui-state-highlight').removeClass("ui-state-highlight")
 				$(this).addClass("ui-state-highlight")
 
 		}
 	});
-	 
-})
+}
